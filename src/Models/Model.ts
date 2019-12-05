5250490ad3c4af09520caeaffd3f38f873ee4f15
La Quốc Thắng <quocthang0507@gmail.com>
2019-12-05 20:53:59 +0700

    Update project

--- src/Models/Model.ts
+++ src/Models/Model.ts
@@ -1,50 +1,58 @@
 import { Account } from "./Account";
 import { Injectable } from "@angular/core";
 import { Storage } from '@ionic/storage';
+import { Platform } from "ionic-angular";
 
 @Injectable()
 export class Model {
     private list: Account[] = [];
     private current: Account;
 
-    constructor(public storage: Storage) {
+    constructor(public storage: Storage, public platform: Platform) {
         console.log('Model: Model created');
+    }
+
+    start(): void {
+        this.getDataFromStorage().then(result => {
+            if (result != null) {
+                let temp = JSON.parse(result);
+                this.list = [];
+                temp.forEach(element => {
+                    this.list.push(new Account(element.id, element.password, element.amount));
+                });
+            }
+        });
         this.add(new Account("laquocthang", "1234", 100000));
     }
 
-    public readDataFromStorage() {
-        this.storage.get('list').then(result => {
-            if (!result) {
-                this.list = JSON.parse(result);
-            }
-        });
+    public getDataFromStorage(): Promise<any> {
         console.log('Model: Read data from storage successfully');
+        return this.storage.get('list');
     }
 
     public removeDataFromStorage(): Promise<any> {
+        console.log('Model: Remove data from storage successfully');
         return this.storage.remove('list');
     }
 
     public saveDataToStorage() {
-        this.removeDataFromStorage();
         this.storage.set('list', JSON.stringify(this.list));
         console.log('Model: Save data to storage successfully');
     }
 
     public add(account: Account) {
         let success: Boolean;
-        this.readDataFromStorage();
         if (this.find(account.getId()) == null) {
             this.list.push(account);
             console.log('Model: I will add new account because there are no existing same account');
-            this.saveDataToStorage();
             success = true;
         }
         else {
             console.log('Model: I won\'t add new account because there are existing account');
             success = false;
         }
-        console.log('Model: # existing users: ' + this.list.length);
+        console.log('Model: # users: ' + this.list.length);
+        this.saveDataToStorage();
         return success;
     }
 
@@ -60,7 +68,7 @@ export class Model {
     }
 
     public find(id: string) {
-        this.readDataFromStorage();
+
         console.log('Model: Looking for account...');
         for (let i = 0; i < this.list.length; i++) {
             if ((this.list[i] as Account).getId() == id) {
@@ -85,9 +93,8 @@ export class Model {
     }
 
     public getAllAccounts() {
-        this.readDataFromStorage();
         let result: string[] = [];
-        console.log('Model: Getting all account name');
+        console.log('Model: Getting all account names');
         this.list.forEach(account => {
             result.push(account.getId());
         });
@@ -95,7 +102,6 @@ export class Model {
     }
 
     public removeAccount(id: string) {
-        this.readDataFromStorage();
         let account: Account = this.find(id);
         if (account == null) {
             console.log('Model: Not found customer so I don\'t remove it');
--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Model } from '../../Models/Model';
 import { Storage } from '@ionic/storage';
+import { Platform } from 'ionic-angular';
 
 /*
   Generated class for the GlobalProvider provider.
@@ -9,12 +10,14 @@ import { Storage } from '@ionic/storage';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
+
 @Injectable()
 export class GlobalProvider {
 	public static model: Model;
 
-	constructor(public storage: Storage) {
+	constructor(public storage: Storage, public platform: Platform) {
 		console.log('Hello GlobalProvider Provider');
-		GlobalProvider.model = new Model(storage);
+		GlobalProvider.model = new Model(storage, platform);
+		GlobalProvider.model.start();
 	}
 }
